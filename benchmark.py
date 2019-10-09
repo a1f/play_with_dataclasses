00ad36588af8ffef87ce6945dfde2cdd4c97b770
Alex Fetisov <alexfetisov@fb.com>
2019-10-08 22:47:57 -0700

    added more comments

--- benchmark.py
+++ benchmark.py
@@ -3,9 +3,9 @@ import time
 from typing import NamedTuple
 import sys
 
-S = 0
+S = 0 # Just global variable for testing the method call.
 
-
+# Our dataclass implementation. Couple of fields and method.
 @dataclass
 class TestDC:
     a: int
@@ -15,7 +15,7 @@ class TestDC:
         global S
         S += self.a
 
-
+# NamedTuple class with couple of fields and method. Same as dataclass.
 class TestNT(NamedTuple):
     a: int
     b: str
@@ -26,6 +26,10 @@ class TestNT(NamedTuple):
 
 print("Benchmarking")
 
+# ------------------------------
+# First we test the creation
+# ------------------------------
+
 start_time = time.time()
 all_dc = [TestDC(i, "a") for i in range(1000000)]
 print("Creation for dataclass: %.5f sec" % (time.time() - start_time))
@@ -33,6 +37,10 @@ print("Creation for dataclass: %.5f sec" % (time.time() - start_time))
 start_time = time.time()
 all_nt = [TestNT(i, "a") for i in range(1000000)]
 print("Creation for NamedTuples: %.5f sec" % (time.time() - start_time))
+
+# ------------------------------
+# Now we check the access
+# ------------------------------
 
 sum = 0
 start_time = time.time()
@@ -46,6 +54,9 @@ for nt in all_nt:
     sum += nt.a
 print("Read for NamedTuples: %.5f sec" % (time.time() - start_time))
 
+# ------------------------------
+# Calling the method
+# ------------------------------
 
 start_time = time.time()
 for dc in all_dc:
@@ -57,6 +68,10 @@ start_time = time.time()
 for nt in all_nt:
     nt.change()
 print("Method for NamedTuples: %.5f sec" % (time.time() - start_time))
+
+# ------------------------------
+# Finally check objects sizes
+# ------------------------------
 
 print("Size of dataclass: %.5f" % sys.getsizeof(all_dc[0]))
 print("Size of NamedTuples: %.5f" % sys.getsizeof(all_nt[0]))
